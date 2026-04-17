33d9a10c463c7654e3c40808fa7c346f9d29b079
Vini-19 <marcosanchez-1@outlook.com>
2026-04-17 05:13:51 -0600

    Mejor del nav y slider

--- Fronted-demoApp/src/app/pages/home/home.component.ts
+++ Fronted-demoApp/src/app/pages/home/home.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from "@angular/router";
+import { Router, RouterLink, RouterOutlet, RouterLinkActive } from "@angular/router";
+import { NgClass, NgIf } from '@angular/common';
 import { AuthService } from '../../services/auth-service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, NgClass,NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   nombreUsuario: string = '';
+  sidebarCollapsed = false;
 
   constructor(
     private authService: AuthService,
@@ -20,6 +22,10 @@ export class HomeComponent {
     private toastr: ToastrService
   ) {
     this.nombreUsuario = this.authService.obtenerNombre() || '';
+  }
+
+  toggleSidebar(): void {
+    this.sidebarCollapsed = !this.sidebarCollapsed;
   }
 
   logout() {
